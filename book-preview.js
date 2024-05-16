da34042efad35d8e12fcdfd3c5777a68480ce5f7
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-16 14:46:50 +0200

    Create component for book-preview.js

--- book-preview.js
+++ book-preview.js
@@ -1,21 +1,28 @@
-// book-preview.js
-import { html, LitElement, property, customElement } from 'lit-element';
-
-@customElement('book-preview')
-class BookPreview extends LitElement {
-  @property({ type: Object }) book;
-
-  render() {
-    return html`
-      <button class="preview" data-preview=${this.book.id}>
-        <img class="preview__image" src=${this.book.image} />
-        <div class="preview__info">
-          <h3 class="preview__title">${this.book.title}</h3>
-          <div class="preview__author">${authors[this.book.author]}</div>
-        </div>
-      </button>
-    `;
+class BookPreview extends HTMLElement {
+    constructor() {
+      super();
+  
+      // Create a shadow DOM
+      this.attachShadow({ mode: "open" });
+  
+      // Clone the template and attach it to the shadow DOM
+      const template = document.getElementById("book-preview-template");
+      const templateContent = template.content.cloneNode(true);
+      this.shadowRoot.appendChild(templateContent);
+  
+      // Get references to elements inside the shadow DOM
+      this.imageElement = this.shadowRoot.querySelector(".preview__image");
+      this.titleElement = this.shadowRoot.querySelector(".preview__title");
+      this.authorElement = this.shadowRoot.querySelector(".preview__author");
+    }
+  
+    connectedCallback() {
+      // Set attributes and text content based on properties
+      this.imageElement.setAttribute("src", this.getAttribute("image"));
+      this.titleElement.textContent = this.getAttribute("title");
+      this.authorElement.textContent = this.getAttribute("author");
+    }
   }
-}
-
-export default BookPreview;
+  
+  // Define the custom element
+  customElements.define("book-preview", BookPreview);
