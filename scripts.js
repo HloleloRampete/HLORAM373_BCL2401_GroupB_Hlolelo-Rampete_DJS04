e019489164c18d7b23c100ad1a4e7706de87c788
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-17 15:58:24 +0200

    Refactoring the new createBookPreview arrow function

--- scripts.js
+++ scripts.js
@@ -12,19 +12,10 @@ const getElement = (selector) => document.querySelector(selector);
 // Function to create and append book previews in the DOM
 const createBookPreviews = (books, container) => {
   const fragment = document.createDocumentFragment();
-  books.forEach(({ author, id, image, title }) => {
-    const element = document.createElement("button");
-    element.classList = "preview";
-    element.setAttribute("data-preview", id);
-    element.innerHTML = `
-            <img class="preview__image" src="${image}" />
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `;
-
-    fragment.appendChild(element);
+  books.forEach((book) => {
+    const preview = new BookPreview();
+    preview.book = book;
+    fragment.appendChild(preview);
   });
   container.appendChild(fragment);
 };
