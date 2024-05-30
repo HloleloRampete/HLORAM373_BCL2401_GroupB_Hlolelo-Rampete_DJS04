008cf10ec39a1a37ee8a738d284b0cf1a3ee856b
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-30 23:24:29 +0200

    "Added createBookPreviews function and reformatted getElement function"

--- functions.js
+++ functions.js
@@ -1,1 +1,14 @@
 export const getElement = (selector) => document.querySelector(selector);
+
+export const createBookPreviews = (books, container) => {
+    const fragment = document.createDocumentFragment();
+    books.forEach((book) => {
+      const preview = document.createElement("book-preview");
+      preview.dataset.author = book.author;
+      preview.dataset.id = book.id;
+      preview.dataset.image = book.image;
+      preview.dataset.title = book.title;
+      fragment.appendChild(preview);
+    });
+    container.appendChild(fragment);
+  };
