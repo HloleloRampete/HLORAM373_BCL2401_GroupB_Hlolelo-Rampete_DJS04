81c8f6586c51600898fc037458e4609c45fcef30
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-05 09:40:43 +0200

    "Refactor BookPreview class: updated constructor, render method, and styles; changed attribute handling and added template for HTML structure"

--- bookPreview.js
+++ bookPreview.js
@@ -1,85 +1,96 @@
-// book-preview.js
+//import { books, authors, genres } from './data.js';
+
 import { authors } from "./data.js";
-class BookPreview extends HTMLElement {
-  connectedCallback() {
-    this.render();
-  }
-  render() {
-    const { author, id, image, title } = this.dataset;
-    const authoimport { authors } from "../data";
-    class BookPreview extends HTMLElement {
-        constructor() {
-          super();
-          this.shadowRoot = this.attachShadow({ mode: "open" });
-          this.handleClick = this.handleClick.bind(this);
-        }
-      
-        static get observedAttributes() {
-          return ["book"];
-        }
-      
-        attributeChangedCallback(name, oldValue, newValue) {
-          if (name === "book") {
-            this.render(JSON.parse(newValue));
-          }
-        }
-      
-        render(book) {
-          this.shadowRoot.innerHTML = `
-            <button class="preview" data-preview="${book.id}">
-              <img class="preview__image" src="${book.image}" alt="${book.title}" />
-              <div class="preview__info">
-                <h3 class="preview__title">${book.title}</h3>
-                <div class="preview__author">${book.author}</div>
-              </div>
+
+export class BookPreview extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback(){
+        this.render()
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render()
+    }
+
+    static get observedAttribute() {
+        return ['author', 'id', 'image', 'title'];
+    }
+
+    render() {
+        // const {author, id, image, title} = this.dataset;
+        const author = this.getAttribute('author');
+        const id = this.getAttribute('id');
+        const image = this.getAttribute('image');
+        const title = this.getAttribute('title');
+
+        const template = document.createElement('template');
+
+        template.innerHTML = `
+            <style>
+              .preview {
+                    border-width: 0;
+                    width: 100%;
+                    font-family: Roboto, sans-serif;
+                    padding: 0.5rem 1rem;
+                    display: flex;
+                    align-items: center;
+                    cursor: pointer;
+                    text-align: left;
+                    border-radius: 8px;
+                    border: 1px solid rgba(var(--color-dark), 0.15);
+                    background: rgba(var(--color-light), 1);
+                }
+                @media (min-width: 60rem) {
+                  .preview {
+                        padding: 1rem;
+                    }
+                }
+              .preview_hidden {
+                    display: none;
+                }
+              .preview:hover {
+                    background: rgba(var(--color-blue), 0.05);
+                }
+              .preview__image {
+                    width: 48px;
+                    height: 70px;
+                    object-fit: cover;
+                    background: grey;
+                    border-radius: 2px;
+                    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+                                0px 1px 1px 0px rgba(0, 0, 0, 0.1),
+                                0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+                }
+              .preview__info {
+                    padding: 1rem;
+                }
+              .preview__title {
+                    margin: 0 0 0.5rem;
+                    font-weight: bold;
+                    display: -webkit-box;
+                    -webkit-line-clamp: 2;
+                    -webkit-box-orient: vertical;
+                    overflow: hidden;
+                    color: rgba(var(--color-dark), 0.8);
+                }
+              .preview__author {
+                    color: rgba(var(--color-dark), 0.4);
+                }
+            </style>
+            <button class="preview" data-preview="${id}">
+                <img class="preview__image" src="${image}"/>
+                <div class="preview__info">
+                    <h3 class="preview__title">${title}</h3>
+                    <div class="preview__author">${authors[author]}</div>
+                </div>
             </button>
-          `;
-      
-          this.shadowRoot.querySelector("button").addEventListener("click", this.handleClick);
-        }
-      
-        handleClick(event) {
-          const previewId = event.currentTarget.dataset.preview;
-          // Trigger an event to notify the application about the clicked preview
-          this.dispatchEvent(new CustomEvent("book-clicked", { detail: { previewId } }));
-        }
-      }
-      
-      customElements.define("book-preview", BookPreview);
-      rName = authors[author]; // Fetch the author's name using the ID
+        `;
 
-    this.innerHTML = `
-      <style>
-        .preview {
-          display: flex;
-          align-items: center;
-        }
-        .preview__image {
-          width: 50px;
-          height: 75px;
-          margin-right: 10px;
-        }
-        .preview__info {
-          display: flex;
-          flex-direction: column;
-        }
-        .preview__title {
-          font-size: 1.2em;
-        }
-        .preview__author {
-          font-size: 1em;
-          color: gray;
-        }
-      </style>
-      <button class="preview" data-preview="${id}">
-        <img class="preview__image" src="${image}" />
-        <div class="preview__info">
-          <h3 class="preview__title">${title}</h3>
-          <div class="preview__author">${authorName}</div> <!-- Use the author's name here -->
-        </div>
-      </button>
-    `;
-  }
+        this.shadowRoot.innerHTML = '';
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
 }
-
-customElements.define("book-preview", BookPreview);
